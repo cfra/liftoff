07f6e4465ec694d649b3bb192565211ff51daa26
Torben Schloß <torben.schloss@gmx.de>
2016-07-31 19:05:09 +0000

    Update 1.1

--- liftoff/motor.ino
+++ liftoff/motor.ino
@@ -6,18 +6,20 @@ void goUp() {
     digitalWrite(L2,    HIGH);
     digitalWrite(up,    LOW);
     digitalWrite(upSR,  LOW);
+    digitalWrite(Lamp,    LOW); //Lampe ein
   }
   else {
     off();
   }
 }
-
+// L1 L2 aendern
 void goDown() {
   if (signalRead(P3) != 1) {
     digitalWrite(L1,      HIGH);
     digitalWrite(L2,      HIGH);
     digitalWrite(down,    LOW);
     digitalWrite(downSR,  LOW);
+    digitalWrite(Lamp,    LOW); //Lampe ein
   }
   else {
     off();
@@ -34,6 +36,7 @@ void off() {
   digitalWrite(downSR,  HIGH);
   digitalWrite(L1,      LOW);
   digitalWrite(L2,      LOW);
+  digitalWrite(Lamp,    HIGH); //Lampe aus
   Serial.println(millis() - startTime);
   currentTime = 0; //reset der laufzeit
   delay(251); //die Relais duerfen den Motor darf nicht zu schnell schalten (250ms)
